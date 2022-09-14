409cfeee2b65c7de62ab245539e8658e219a06b2
Piyush <cpiyush170@gmail.com>
2022-09-14 15:54:18 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
 
 const indexRouter = require('./routes/index');
-
+const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 // setting view engine as ejs 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 // importing mongoose (ODM)
 const mongoose = require('mongoose');
 
@@ -32,5 +35,6 @@ mongoose.connect(uri, options).then(() => {
 
 
 app.use('/', indexRouter);
-
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 app.listen(process.env.PORT || 3000)
