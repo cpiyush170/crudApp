73f9e0d62ea76e6921d79412c62e475c7ef56d33
Piyush <cpiyush170@gmail.com>
2022-12-30 15:44:59 +0530

    author route done

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
@@ -6,5 +7,21 @@ const authorSchema = new mongoose.Schema({
         required: true
     }
 });
+
+// this code make sure that authors with books doesn't get deleted
+authorSchema.pre('remove', function (next) {
+    Book.find({ author: this.id }, (err, books) => {
+        if (err) {
+            next(err)
+        }
+        else if (books.length > 0) {
+            next(new Error('This author has books, you are not allowed to delete'))
+        }
+        else {
+            next()
+        }
+    })
+})
+
 
 module.exports = mongoose.model('Author', authorSchema);
